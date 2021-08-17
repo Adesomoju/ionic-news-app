e39cccabdcdadf7fb740eba91adfc5cddba5fccf
unknown <adesomojufisayo@gmail.com>
2021-08-17 14:59:56 +0100

    initial commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-folder',
@@ -7,12 +9,30 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+  author: any;
+  key = environment.api_key;
+  selectedContent
+  content
+  firstContent
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService
+  ) { }
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.getNews()
+  }
+
+  async getNews(){
+    await this.apiService.get(`v2/top-headlines?country=us&apiKey=${this.key}`)
+    .subscribe(async(res) =>{
+      this.content = res.articles;
+      this.firstContent = res.articles[0];
+      this.selectedContent = this.content.slice(10)
+      console.log(this.firstContent)
+    })
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_url: 'https://newsapi.org',
+  api_key: '469fa81af0ed493c85e555d633f1d2f8'
 };
 
 /*
